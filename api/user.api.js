3e2f66910b3ec9b2adde88f2eb830894106d8126
vaibhav joshi <vaibhavj143.vj.vj@gmail.com>
2022-11-04 10:38:50 +0530

    port

--- api/user.api.js
+++ api/user.api.js
@@ -2,6 +2,7 @@ import User from "../model/user.model.js";
 
 export const createUser = async (req, res) => {
     try {
+      console.log(req);
       let reqUser = req.body;
       if (reqUser.password.length > 18 || reqUser.password.length < 4) {
         res.status(200).send("password length 6min 18max");
@@ -59,16 +60,13 @@ export const createUser = async (req, res) => {
   export const loginUser = async (req, res) => {
     try {
       const { email, password } = req.body;
-  
       const user = await User.findOne({ email }).select("+password");
-  
       if (!user) {
         return res.status(400).send({
           success: false,
           message: "user dose not exist",
         });
       }
-  
       const isMatch = await user.matchPassword(password);
   
       if (!isMatch) {
@@ -77,12 +75,9 @@ export const createUser = async (req, res) => {
           message: "Invalid mail or password",
         });
       }
-  
       // console.log("token");
       const token = await user.generateToken();
-  
       // console.log("tokenEnd");
-  
       const options = {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
         httpOnly: true,
--- src/index.js
+++ src/index.js
@@ -1,56 +1,43 @@
-
-
 // const express = require("express");
 
 import express from "express";
 
 import dotenv from "dotenv";
 import cors from "cors";
-import { connectDatabase } from '../config/database.js';
+import { connectDatabase } from "../config/database.js";
 import userRoutes from "../routes/user.routes.js";
-
 
 const app = express();
 
 dotenv.config();
 // middleware  handlers  express routes
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-
-app.get("/",async (req,res)=>{
-
+app.get("/", async (req, res) => {
   try {
-    
+    console.log("hi");
+
     res.status(200).send({
-      success:true,
-      message:"all set"
-    })
-
+      success: true,
+      message: "all set",
+    });
   } catch (error) {
-    res.status(500).send({
-      success:false,
-      message:"not good"
-    })
+    res.status(500).json({
+      success: false,
+      message: "not good",
+    });
   }
+});
 
-})
+app.use("/api/v1", userRoutes);
 
-
-app.use("/api/v1" , userRoutes);
-
-
-
-
-app.listen(process.env.PORT|| '5050', async () => {
+app.listen(process.env.PORT || "5050", async () => {
   try {
-
     await connectDatabase();
-
-    console.log(`http://localhost:${process.env.PORT|| '5050'}`);
+    console.log(`http://localhost:${process.env.PORT || "5050"}`);
   } catch (error) {
     console.log(`${error.message}`);
   }
